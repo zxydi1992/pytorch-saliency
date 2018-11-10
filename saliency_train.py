6734b4ad79e444a7631367641f826776a454bd9e
zhangxinyang <zxydi1992@hotmail.com>
2018-11-10 16:30:41 +0800

    fix a bug.

--- saliency_train.py
+++ saliency_train.py
@@ -38,7 +38,7 @@ def black_box_densenet169(cuda=True):
 
     black_box_model.train(False)
     if cuda:
-        black_box_model = torch.nn.DataParallel(black_box_model)
+        black_box_model = black_box_model.cuda()
         # black_box_model = torch.nn.DataParallel(black_box_model).cuda()
 
     def black_box_fn(_images):
