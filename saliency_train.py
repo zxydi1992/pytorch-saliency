08ab905639aab65c12c5fe6c9f69df8f482d55bc
zhangxinyang <zxydi1992@hotmail.com>
2018-11-10 21:51:09 +0800

    check in.

--- saliency_train.py
+++ saliency_train.py
@@ -49,7 +49,6 @@ def black_box_densenet169(cuda=True):
     freeze_model(black_box_model)
 
     def black_box_fn(_images):
-        print(torch.min(_images[0]), torch.max(_images[0]))
         return black_box_model(imagenet_normalize(0.5 * (_images + 1)))
     return black_box_fn
 
