correct normalization.

saliency_train.py
@@ -50,7 +50,7 @@ def black_box_densenet169(cuda=True):
 
     def black_box_fn(_images):
         print(torch.min(_images[0]), torch.max(_images[0]))
-        return black_box_model(imagenet_normalize(_images * 0.5 + 1))
+        return black_box_model(imagenet_normalize(0.5 * (_images + 1)))
     return black_box_fn
 
 