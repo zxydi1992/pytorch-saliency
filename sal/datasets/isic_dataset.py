69e33a1c1a1f75f5c4f40a0acebc4d8145d4a7a6
zhangxinyang <zxydi1992@hotmail.com>
2019-02-13 14:41:04 -0500

    adapt RTS for ISIC model

---
+++ sal/datasets/isic_dataset.py
@@ -0,0 +1,99 @@
+import os
+import pickle
+import csv
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import (
+    Compose, CenterCrop, ToTensor, RandomHorizontalFlip, RandomSizedCrop,
+    ColorJitter, RandomVerticalFlip
+)
+
+
+ISIC_EVAL_TRANSFORMS = Compose([
+                                CenterCrop(224),
+                                ToTensor()]
+                               )
+ISIC_IMAGES_DIR = '/home/xinyang/Data/intattack/ISIC-2018/data/isic/task3/images/HAM10000/'
+ISIC_LABEL_PATH = '/home/xinyang/Data/intattack/ISIC-2018/data/isic/task3/labels/HAM10000/labels.csv'
+ISIC_CV_SPLIT_PATH = '/home/xinyang/Data/intattack/ISIC-2018/data/isic/indices_new.pkl'
+
+ISIC_RESNET50_CKPT_PATH = '/home/xinyang/Data/intattack/ISIC-2018/data/isic/ISIC.example_resnet50_5foldcv/CVSet1/checkpoint_best-65.pt'
+
+
+def read_metadata(path):
+    reader = csv.reader(open(path))
+    it = iter(reader)
+    next(it)
+    images, labels = [], []
+    for row in it:
+        image, label = row[0], [int(float(v)) for v in row[1:]]
+        label = label.index(1)
+        images.append(image + '.jpg')
+        labels.append(label)
+    return images, np.asarray(labels, np.int64)
+
+
+def get_isic_train_transform():
+    transforms = Compose([
+                        RandomSizedCrop(224),
+                        RandomHorizontalFlip(),
+                        RandomVerticalFlip(),
+                        ColorJitter(brightness=32. / 255.,saturation=0.5),
+                        ToTensor()
+    ])
+    return transforms
+
+
+def get_isic_val_transform():
+    transforms = Compose([
+                         CenterCrop(224),
+                         ToTensor()
+    ])
+    return transforms
+
+
+def get_train_dataset(size=224):
+    with open(ISIC_CV_SPLIT_PATH, 'rb') as f:
+        obj = pickle.load(f)
+    indices = obj['trainIndCV'][1]
+    return ISICDataset(ISIC_IMAGES_DIR, None, indices, transforms=get_isic_train_transform())
+
+
+def get_val_dataset(size=224):
+    with open(ISIC_CV_SPLIT_PATH, 'rb') as f:
+        obj = pickle.load(f)
+    indices = obj['valIndCV'][1]
+    return ISICDataset(ISIC_IMAGES_DIR, None, indices, transforms=get_isic_val_transform())
+
+
+def get_loader(dataset, batch_size=64, pin_memory=True):
+    return DataLoader(dataset=dataset, batch_size=batch_size,
+                      shuffle=True, drop_last=True, num_workers=4, pin_memory=pin_memory)
+
+
+class ISICDataset(Dataset):
+
+    def __init__(self, images_dir=None, label_path=None, indices=None, transforms=None):
+        if images_dir is None:
+            images_dir = ISIC_IMAGES_DIR
+        self.image_dir = images_dir
+        if label_path is None:
+            label_path = ISIC_LABEL_PATH
+        self.image_paths, self.labels = read_metadata(label_path)
+        if indices is not None:
+            self.image_paths = [self.image_paths[i] for i in indices]
+            self.labels = self.labels[indices]
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        path = os.path.join(self.image_dir, self.image_paths[index])
+        image = Image.open(path)
+        if self.transforms is not None:
+            image = self.transforms(image)
+        label = self.labels[index]
+        return image, label, path
---
+++ saliency_train_isic.py
@@ -0,0 +1,125 @@
+import argparse
+import torch
+from sal.utils.pytorch_fixes import *
+from sal.utils.pytorch_trainer import *
+from sal.saliency_model import SaliencyModel, SaliencyLoss
+from sal.utils.resnet_encoder_isic import resnet50encoder
+
+from sal.datasets.isic_dataset import ISIC_RESNET50_CKPT_PATH
+import sal.datasets.isic_dataset as isic_dataset
+
+
+
+# ---- config ----
+# You can choose your own dataset and a black box classifier as long as they are compatible with the ones below.
+# The training code does not need to be changed and the default values should work well for high resolution ~300x300 real-world images.
+# By default we train on 224x224 resolution ImageNet images with a resnet50 black box classifier.
+dts = isic_dataset
+# black_box_fn = get_black_box_fn(model_zoo_model=densenet169)
+# ----------------
+
+
+def freeze_model(model):
+    for param in model.parameters():
+        param.requires_grad_(False)
+
+
+def black_box_resnet_isic(cuda=True, ckpt_path=None):
+    black_box_model = resnet50encoder()
+
+    if cuda:
+        black_box_model.cuda()
+    if ckpt_path is not None:
+        black_box_model.load_state_dict(
+            (torch.load(ckpt_path, map_location=lambda storage, loc: storage)['state_dict'])
+        )
+    black_box_model.train(False)
+    freeze_model(black_box_model)
+
+    def black_box_fn(_images):
+        return black_box_model(_images)[0]
+    return black_box_fn
+
+
+train_dts = dts.get_train_dataset()
+val_dts = dts.get_val_dataset()
+
+encoder = resnet50encoder(7)
+encoder.load_state_dict(torch.load(ISIC_RESNET50_CKPT_PATH)['state_dict'])
+# Default saliency model with pretrained resnet50 feature extractor, produces saliency maps which have resolution 4 times lower than the input image.
+saliency = SaliencyModel(encoder, 5, 64, 3, 64, fix_encoder=True, use_simple_activation=False, allow_selector=True, num_classes=7)
+
+saliency_p = saliency.cuda()
+saliency_loss_calc = SaliencyLoss(black_box_resnet_isic(ckpt_path=ISIC_RESNET50_CKPT_PATH),
+                                  smoothness_loss_coef=0.005) # model based saliency requires very small smoothness loss and therefore can produce very sharp masks
+optim_phase1 = torch_optim.Adam(saliency.selector_module.parameters(), 0.001, weight_decay=0.0001)
+optim_phase2 = torch_optim.Adam(saliency.get_trainable_parameters(), 0.001, weight_decay=0.0001)
+
+@TrainStepEvent()
+@EveryNthEvent(4000)
+def lr_step_phase1(s):
+    print()
+    print(GREEN_STR % 'Reducing lr by a factor of 10')
+    for param_group in optim_phase1.param_groups:
+        param_group['lr'] = param_group['lr'] / 10.
+
+
+@ev_batch_to_images_labels
+def ev_phase1(_images, _labels):
+    __fakes = Variable(torch.Tensor(_images.size(0)).uniform_(0, 1).cuda()<FAKE_PROB)
+    _targets = (_labels + Variable(torch.Tensor(_images.size(0)).uniform_(1, 999).cuda()).long()*__fakes.long())%1000
+    _is_real_label = PT(is_real_label=(_targets == _labels).long())
+    _masks, _exists_logits, _ = saliency_p(_images, _targets)
+    PT(exists_logits=_exists_logits)
+    exists_loss = F.cross_entropy(_exists_logits, _is_real_label)
+    loss = PT(loss=exists_loss)
+
+
+@ev_batch_to_images_labels
+def ev_phase2(_images, _labels):
+    __fakes = Variable(torch.Tensor(_images.size(0)).uniform_(0, 1).cuda()<FAKE_PROB)
+    _targets = PT(targets=(_labels + Variable(torch.Tensor(_images.size(0)).uniform_(1, 999).cuda()).long()*__fakes.long())%1000)
+    _is_real_label = PT(is_real_label=(_targets == _labels).long())
+    _masks, _exists_logits, _ = saliency_p(_images, _targets)
+    PT(exists_logits=_exists_logits)
+    saliency_loss = saliency_loss_calc.get_loss(_images, _labels, _masks, _is_real_target=_is_real_label,  pt_store=PT)
+    loss = PT(loss=saliency_loss)
+
+
+FAKE_PROB = -1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('phase', choices=[1, 2], type=int)
+    parser.add_argument('save_dir')
+    parser.add_argument('--load-model', dest='load_model')
+    config = parser.parse_args()
+    print('config', config)
+
+    if config.load_model is not None:
+        saliency.minimialistic_restore(config.load_model)
+    saliency_old = saliency
+    saliency = nn.DataParallel(saliency)
+
+    if config.phase == 1:
+        nt_phase1 = NiceTrainer(ev_phase1, dts.get_loader(train_dts, batch_size=128), optim_phase1,
+                         val_dts=dts.get_loader(val_dts, batch_size=128),
+                         modules=[saliency],
+                         printable_vars=['loss', 'exists_accuracy'],
+                         events=[lr_step_phase1,],
+                         computed_variables={'exists_accuracy': accuracy_calc_op('exists_logits', 'is_real_label')})
+        FAKE_PROB = .5
+        nt_phase1.train(8500)
+    else:
+        nt_phase2 = NiceTrainer(ev_phase2, dts.get_loader(train_dts, batch_size=64), optim_phase2,
+                         val_dts=dts.get_loader(val_dts, batch_size=64),
+                         modules=[saliency],
+                         printable_vars=['loss', 'exists_accuracy'],
+                         events=[],
+                         computed_variables={'exists_accuracy': accuracy_calc_op('exists_logits', 'is_real_label')})
+        FAKE_PROB = .3
+        nt_phase2.train(3000)
+
+    saliency_old.cpu()
+    saliency_old.minimalistic_save(config.save_dir)  # later to restore just use saliency.minimalistic_restore methdod.
